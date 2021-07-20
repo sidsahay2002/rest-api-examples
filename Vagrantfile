411cf4cbfc411deda6d5573e4f048d3c47a6f426
Siddhant Sahay <sidsahay2002@gmail.com>
2021-07-20 21:35:28 +0530

    Added put, post, patch and delete methods to our APIView

--- Vagrantfile
+++ Vagrantfile
@@ -16,6 +16,9 @@ Vagrant.configure("2") do |config|
  config.vm.box_version = "~> 20200304.0.0"
 
  config.vm.network "forwarded_port", guest: 8000, host: 8000
+ if Vagrant.has_plugin?("vagrant-timezone")
+    config.timezone.value = "Asia/Kolkata"
+ end
 
  config.vm.provision "shell", inline: <<-SHELL
    systemctl disable apt-daily.service
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -14,7 +17,32 @@ class HelloApiView(APIView):
             'Gives you the most control over your logic',
             'Is mapped manually to URLs',
         ]
-        name = self.request.query_params.get('name')
+        """name = self.request.query_params.get('name')
+        age = self.request.query_params.get('age')"""
 
-        """return Response({'message': 'Hello!', 'an_apiview': an_apiview, 'Name': name})"""
-        return Response("my name is " + name)
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+        """return Response([344, 4556, 223, 223])"""
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def put(self, request, pk=None):
+        """Handle updating oan object"""
+        return Response({'methhod:':'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'Patch'})
+
+    def delete(self, request, pk= None):
+        """Delete an object"""
+        return Response({'method': 'delete'})
